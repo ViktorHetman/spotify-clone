bbb2544d5304bacb214f1391cd59dd1f52d493ef
Viktor Hetman <vitya.getman.79@gmail.com>
2023-03-08 10:58:55 +0200

    created component for controlling the volume

---
+++ src/Components/VolumeController.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+import React from 'react'
+
+function VolumeController() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+
+`
+
+export default VolumeController
+
+//https://api.spotify.com/v1/me/player/volume = api call
