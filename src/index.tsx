9a926b2f61c52046ab11ee2fd2775da2821a93bf
Viktor <vitya.getman.79@gmail.com>
2023-05-31 14:00:12 +0300

    setting up store and types for dispatch thunk and selector

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App';
+
+import { store } from './redux/store';
 
 import { StateProvider } from './utils/StateProvider.jsx';
 import reducer, { initialState } from './utils/reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <StateProvider initialState={initialState} reducer={reducer}>
-    <App />
-  </StateProvider>
+  <Provider store={store}>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </Provider>
 );
